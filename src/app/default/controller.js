8d904ca14d473f0bc9e6c0a4ac489a7cc4adea3a
nms377 <nms377@nyu.edu>
2017-03-15 21:33:59 -1000

    Setup Default state, setup barrel files, setup template, setup controller.

---
+++ src/app/default/controller.js
@@ -0,0 +1,6 @@
+export const DefaultCtrl = class DefaultCtrl {
+	constructor() {
+		this.title = "Reddit 4.1.potato";
+
+	}
+};
---
+++ src/app/default/state.js
@@ -0,0 +1,9 @@
+const template = require('./template.html');
+
+export const DefaultState = {
+	name: 'default',
+	url: '/',
+	template,
+	controller: 'DefaultCtrl',
+	controllerAs: 'default'
+};
